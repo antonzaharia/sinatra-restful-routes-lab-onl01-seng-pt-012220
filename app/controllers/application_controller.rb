602b694d076bbf7752b495b15e4b5ee503c01fce
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-04-23 16:44:40 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,11 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
+  get '/' do
+    @recipes = Recipe.all
+    erb :index
+  end
+
   get '/recipes' do
     @recipes = Recipe.all
     erb :index
@@ -16,8 +21,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/recipes' do
-    @recipe = Recipe.create(:name => params[:name], :ingredients => params[:ingredients], :cook_time => params[:params])
-
+    @recipe = Recipe.create(:name => params[:name], :ingredients => params[:ingredients], :cook_time => params[:cook_time])
     redirect to("/recipes/#{@recipe.id}")
   end
 
@@ -26,7 +30,11 @@ class ApplicationController < Sinatra::Base
     erb :show
   end
 
-
+  delete '/recipes/:id/delete' do
+    @recipe = Recipe.find_by_id(params[:id])
+    @recipe.delete
+    redirect to "/"
+  end
 
 
 end
