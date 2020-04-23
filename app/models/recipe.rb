7847e479d305ea58b3eb935356fa13d24932967d
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-04-23 15:56:03 +0000

    Automatically backed up by Learn

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,12 +2,12 @@
 class Recipe <ActiveRecord::Base
   attr_accessor :name, :ingredients, :cook_time
 
-  @@all = []
-
-  def initialize
-    @@all << self
-  end
-  def self.all
-    @@all
-  end
+  # @@all = []
+  #
+  # def initialize
+  #   @@all << self
+  # end
+  # def self.all
+  #   @@all
+  # end
 end
